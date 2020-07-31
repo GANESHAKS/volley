9637a0c89c0834ec959e7f6e7efcbb922c23fd90
GANESHAKS <44470364+GANESHAKS@users.noreply.github.com>
2020-07-31 20:06:34 +0530

    1st

---
+++ app/src/main/java/com/pro/volley/Intents.java
@@ -0,0 +1,4 @@
+package com.pro.volley;
+
+public class Intents {
+}
--- app/src/main/java/com/pro/volley/MainActivity.java
+++ app/src/main/java/com/pro/volley/MainActivity.java
@@ -2,7 +2,18 @@ package com.pro.volley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
